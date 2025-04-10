c71c5c9af5e3513b6cef0f91e9a22599a39b27a7
MatheusMiraEsc <mme@cesar.school>
2025-04-09 23:12:34 -0300

    Adição de projeto

---
+++ ingressart/src/main/java/grupo3/ingressart/IngressartApplication.java
@@ -0,0 +1,13 @@
+package grupo3.ingressart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngressartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngressartApplication.class, args);
+	}
+
+}
